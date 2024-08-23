e049bdb489ad55faaa6458730c2b98decc69a900
Cristian Loera <loera_73@hotmail.com>
2024-08-22 20:39:34 -0600

    all the books linked to the review

--- client/src/pages/SingleBook.jsx
+++ client/src/pages/SingleBook.jsx
@@ -24,9 +24,9 @@ const SingleBook = () => {
   return (
     <div className="my-3">
       <h3 className="card-header bg-dark text-light p-2 m-0">
-        {book.bookAuthor} <br />
+        {book.bookTitle} <br />
         <span style={{ fontSize: "1rem" }}>
-          had this book on {book.createdAt}
+          {book.addedBy} had this book on {book.createdAt}
         </span>
       </h3>
       <div className="bg-light py-4">
@@ -38,9 +38,7 @@ const SingleBook = () => {
             border: "2px dotted #1a1a1a",
             lineHeight: "1.5",
           }}
-        >
-          {book.bookText}
-        </blockquote>
+        ></blockquote>
       </div>
 
       <div className="my-5">
